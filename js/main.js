f59c8a39fdb5a123e6ddb519a4b74b9606b9d1e3
Boris <48370748+BorisSlonov@users.noreply.github.com>
2020-05-22 17:48:15 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.header__list'),
-    menuItem = document.querySelectorAll('.header__link'),
-    hamburger = document.querySelector('.header__burger');
+        menuItem = document.querySelectorAll('.header__link'),
+        hamburger = document.querySelector('.header__burger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('burger_active');
@@ -15,3 +15,62 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+function outputUpdate(vol) {
+    var output = document.querySelector('#volume');
+    output.value = vol;
+
+}
+
+
+
+jQuery(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $('.header').addClass('bgc-fff');
+            $('.header').removeClass('bgc-fff-no');
+        }
+        else {
+            $('.header').addClass('bgc-fff-no');
+        }
+    });
+
+
+
+
+    //calc
+
+    jQuery('#fader').change(function () {
+
+
+        var units = jQuery('#volume').val();
+        var cost = 250;
+        if (units >= 2) {
+            var cost = 175;
+        }
+        if (units >= 4) {
+            var cost = 165;
+        }
+        if (units >= 8) {
+            var cost = 160;
+        }
+        if (units >= 16) {
+            var cost = 155;
+        }
+        if (units >= 32) {
+            var cost = 150;
+        }
+
+
+        var score = parseFloat(cost * units);
+        score = units + " " + "шт" + " " + score + " " + "₽";
+        jQuery('.output label').text(score);
+        jQuery('.output').show();
+    });
+
+
+
+});
+
+
